69399cf7af17fb33e4212da7a67060a6f919da54
wangc <blankeeee@gmail.com>
2016-01-19 10:43:31 +0800

    增加note.mk笔记

---
+++ note.mk
@@ -0,0 +1,56 @@
+android-studio必备插件： adb-wifi,butterknife,gsonformat,selector,
+
+
+1.FloatingActionButton
+https://github.com/makovkastar/FloatingActionButton
+
+2.MD
+http://blog.csdn.net/column/details/sunzxyongmaterial.html
+
+3.LayoutTransition 属性动画
+http://www.jianshu.com/p/a03013850ccb
+
+4.补间动画
+http://www.cnblogs.com/yc-755909659/p/4290114.html
+
+5.CircularProgressBar
+https://github.com/castorflex/SmoothProgressBar
+compile 'com.github.castorflex.smoothprogressbar:library-circular:1.2.0'
+
+6.http://jakewharton.github.io/butterknife/
+
+7.Palette  	MD
+http://blog.csdn.net/jdsjlzx/article/details/41441083
+
+8.dagger2
+https://github.com/google/dagger
+https://github.com/fr4nk1/retro-dagger-example
+
+9.mosby
+https://github.com/sockeqwe/mosby
+
+10 百分比布局
+https://github.com/hongyangAndroid/android-percent-support-extend
+自动适配布局
+https://github.com/hongyangAndroid/AndroidAutoLayout
+
+11 设置tag的换肤
+https://github.com/hongyangAndroid/AndroidChangeSkin
+
+12 高亮view
+https://github.com/hongyangAndroid/Highlight
+
+13 状态栏变色
+https://github.com/hongyangAndroid/ColorfulStatusBar
+
+14 textview颜色逐渐变化
+https://github.com/hongyangAndroid/ColorTrackView
+
+15 标签
+https://github.com/kingideayou/TagCloudView
+
+16 无线卡片效果   boss自聘
+http://p.codekk.com/detail/Android/xmuSistone/android-card-slide-panel
+
+17 ASimpleCache缓存
+https://github.com/yangfuhai/ASimpleCache
